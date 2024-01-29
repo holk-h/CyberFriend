7c8ec076ea896ed68ce34c1ca1c4291ce4d75b94
133950 <1339500240@qq.com>
2024-01-29 15:27:06 +0800

    update message_record

---
+++ plugins/message_record/__init__.py
@@ -0,0 +1,44 @@
+import time
+
+from nonebot import get_driver, get_bots, on_message
+from nonebot.plugin import PluginMetadata
+from nonebot.internal.adapter import Bot, Event
+from nonebot.typing import T_State
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="message_record",
+    description="",
+    usage="",
+    config=Config,
+)
+
+from .util import MessageRecordService
+
+global_config = get_driver().config
+config = Config.parse_obj(global_config)
+
+bot = get_bots()
+message_record = on_message(rule=None, priority=10, block=True)
+messageRecordService = MessageRecordService()
+@message_record.handle()
+async def handle_function(bot: Bot, event: Event, state: T_State):
+    # 获取发送人的 QQ 号
+    user_id = event.get_user_id()
+    # 获取发送人的昵称
+    user_name = event.sender.nickname
+    # 获取发送的消息内容
+    message = event.get_message()
+    # 判断是否为群聊消息
+    if event.message_type == "group":
+        # 获取群聊的 ID
+        group_id = event.group_id
+        # 获取群聊的名称
+        # group_info = await bot.get_group_info(group_id=group_id)
+        # group_name = group_info["group_name"]
+        # 输出群聊+人 和 消息内容
+        messageRecordService.addOne(group_id, user_id, str(message), time.time())
+    else:
+        # 输出私聊人 和 消息内容
+        messageRecordService.addOne(user_id, user_id, str(message), time.time())
---
+++ plugins/message_record/util.py
@@ -0,0 +1,48 @@
+import time
+
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///../../message_record.db', echo=False)
+Base = declarative_base()
+session = sessionmaker(bind=engine)()
+
+class MessageRecord(Base):
+    __tablename__ = 'message_record'
+
+    group_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    message = Column(String)
+    data_time = Column(Float, primary_key=True)
+
+    def __repr__(self):
+        return f'<MessageRecord(group_id={self.group_id}, user_id={self.user_id}, message={self.message}, dt={self.data_time})>'
+
+Base.metadata.create_all(engine)
+
+class MessageRecordService:
+    def __init__(self, session=session):
+        self.session=session
+
+    def queryAll(self):
+        return self.session.query(MessageRecord).all()
+
+    def queryLast(self, group_id):
+        return self.session.query(MessageRecord).filter(MessageRecord.group_id==group_id).order_by(MessageRecord.data_time.desc()).limit(15).all()
+
+    def addOne(self,group_id, user_id, message, data_time):
+        messageRecord = MessageRecord(group_id=group_id, user_id=user_id, message=message, data_time=data_time)
+        session.add(messageRecord)
+        session.commit()
+
+if __name__ == '__main__':
+    messageRecordService = MessageRecordService()
+    # for i in range(10):
+    #     messageRecordService.addOne(2, 2, "adsbbb"+str(i), time.time())
+    #     time.sleep(0.1)
+    print(messageRecordService.queryAll())
+    # print(messageRecordService.queryLast(2))
+
+
+
+
