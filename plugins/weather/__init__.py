60b26bf1422983d412617261d652865b8d25bb67
holk-h <me@holk.tech>
2024-01-29 13:51:58 +0800

    update llm

--- plugins/weather/__init__.py
+++ plugins/weather/__init__.py
@@ -1,23 +1,28 @@
-from nonebot import get_driver
-from nonebot.plugin import PluginMetadata
-
-from .config import Config
-
-__plugin_meta__ = PluginMetadata(
-    name="weather",
-    description="",
-    usage="",
-    config=Config,
-)
-
-global_config = get_driver().config
-config = Config.parse_obj(global_config)
-
-from nonebot import on_command
 from nonebot.rule import to_me
+from .utils import GLM
+from nonebot import on_message, get_bots
+from nonebot.rule import to_me
+from nonebot import logger
+from nonebot.internal.adapter import Bot, Event
+from nonebot.rule import to_me
+from nonebot import require
+require("nonebot_plugin_chatrecorder")
+from nonebot_plugin_chatrecorder import get_message_records
+from nonebot_plugin_session import extract_session, SessionIdType
+from datetime import datetime, timedelta
 
-weather = on_command("天气", rule=to_me(), aliases={"weather", "查天气"}, priority=10, block=True)
+bot = get_bots()
+glm = GLM()
+weather = on_message(rule=to_me(), priority=10, block=True)
 
 @weather.handle()
-async def handle_function():
-    await weather.finish("天气是...")
+async def handle_function(bot: Bot, event: Event):
+    session = extract_session(bot, event)
+    records = await get_message_records(
+        session=session,
+        time_start=datetime.utcnow() - timedelta(days=1),
+    )
+    records = records[-15:]
+    records = [i.message[0]['data']['text'] for i in records]
+    logger.warning(records)
+    await weather.finish(glm.call(records, str(event.get_message())))
